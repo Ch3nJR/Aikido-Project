7ba3ed93b3e1a8d80e8dc451644f28c4583ee3d1
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-05-04 10:16:06 +0800

    added scroll to top btn back

--- js/general.js
+++ js/general.js
@@ -4,16 +4,16 @@ $.get("nav.html", function (data) {
 });
 
 //Scroll to top button
-// const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
-// window.addEventListener("scroll", function () {
-//   scrollTopBtn.classList.toggle("active", window.scrollY > 500);
-// });
+window.addEventListener("scroll", function () {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
 
-// scrollTopBtn.addEventListener("click", () => {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// });
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
 //Get footer
 $.get("footer.html", function (data) {
