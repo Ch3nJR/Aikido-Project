b9bc3c83ab52f65f9832eb125ca3b19406152b96
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-05-05 15:13:31 +0800

    scroll reveal added to elements

--- js/about.js
+++ js/about.js
@@ -14,3 +14,33 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+//To create reveal animations
+ScrollReveal({
+  //reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".about-starting", {
+  delay: 500,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".vv-container-text", {
+  delay: 500,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".values", {
+  delay: 600,
+  origin: "bottom",
+  interval: 200,
+});
+
+ScrollReveal().reveal(".teacher-section", {
+  delay: 600,
+  origin: "bottom",
+});
