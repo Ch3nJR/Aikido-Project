18c120f339a0b37ba030134ac644987fc3f17dea
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-05-08 14:25:34 +0800

    scroll reveal added to location

---
+++ js/location.js
@@ -0,0 +1,14 @@
+//To create reveal animations
+ScrollReveal({
+  //reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".location", {
+  delay: 500,
+  origin: "bottom",
+  interval: 200,
+});
