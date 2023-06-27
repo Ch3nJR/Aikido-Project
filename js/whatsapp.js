d2155a4b565336c0890ea6781b8ea34940d8f383
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-06-27 10:49:39 +0800

    removed whatsapp function

--- js/whatsapp.js
+++
@@ -1,6 +0,0 @@
-//whatsapp button
-const whatsappBtn = document.querySelector(".wa-btn");
-
-window.addEventListener("scroll", function () {
-  whatsappBtn.classList.toggle("active", window.scrollY > 100);
-});
