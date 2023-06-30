cbbb7e3d31bfc5955dff2ed7f7f10d3f534f54d9
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-06-30 10:25:47 +0800

    new homepage added

---
+++ js/new.js
@@ -0,0 +1,38 @@
+//Top swiper
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+
+    1300: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 10,
+    },
+
+    1440: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+    },
+  },
+});
