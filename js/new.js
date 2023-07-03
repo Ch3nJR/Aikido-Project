f060fd6dd07d1d6b49b9255a4ab4ae0e26445640
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-07-03 15:15:18 +0800

    completed new homepage

--- js/new.js
+++ js/new.js
@@ -1,6 +1,44 @@
+//To create reveal animations
+ScrollReveal({
+  //reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".left-landing-box, .testimonial-header, .promo-header", {
+  delay: 200,
+  origin: "top",
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".benefit-left, .left", {
+  delay: 500,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".right-landing-box, .benefit-right", {
+  delay: 500,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".feature-container, .testimonial", {
+  delay: 500,
+  origin: "bottom",
+  interval: 200,
+});
+
+ScrollReveal().reveal(".faq-title, .questions-container", {
+  delay: 500,
+  origin: "top",
+});
+
 //Top swiper
 var swiper = new Swiper(".mySwiper", {
   loop: true,
+  slidesPerGroup: 1,
+  spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -13,25 +51,18 @@ var swiper = new Swiper(".mySwiper", {
   breakpoints: {
     320: {
       slidesPerView: 1,
-      slidesPerGroup: 1,
     },
 
     768: {
       slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 15,
     },
 
     1300: {
       slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 30,
     },
 
     1440: {
       slidesPerView: 3,
-      slidesPerGroup: 1,
-      spaceBetween: 30,
     },
   },
 });
